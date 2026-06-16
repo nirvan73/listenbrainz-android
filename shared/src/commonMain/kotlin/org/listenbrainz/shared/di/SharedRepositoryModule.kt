6b1e07073c01df4cf201688eeeee34eea36c64f0
Nirvan Jain <nirvanj73@gmail.com>
2026-06-16 14:28:27 +0530

    moved SocialRepository to shared module and registered it inside sharedRepositoryModule

--- shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
+++ shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
@@ -55,4 +55,5 @@ val sharedRepositoryModule = module {
     single<BPArtistRepository> { BPArtistRepositoryImpl(get(),get(),get()) }
     single<PlaylistDataRepository> { PlaylistDataRepositoryImpl(get(),get(),get(),get(named(IO_DISPATCHER))) }
     single<FeedRepository> { FeedRepositoryImpl(get()) }
+    single<SocialRepository> { SocialRepositoryImpl(get(),get()) }
 }
